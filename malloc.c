c5ded7d96f6014ccef670a59540f1729531356a0
Mahmoud Abdelmoniem <109484204+Mahmoud-Abdelmoneim@users.noreply.github.com>
2024-04-24 11:14:58 -0400

    Add files via upload

--- malloc.c
+++ malloc.c
@@ -13,14 +13,7 @@ free_list *headBlock;
 free_list * current ;
 char FirsttimeFlag=0;
 void * LastLocation ;
-/**
- * @brief Initializes the memory management system.
- *
- * This function initializes the memory management system by setting up the initial
- * free list. It sets the headBlock to the current program break, moves LastLocation
- * to a new location in memory by increasing the program break, and initializes the
- * headBlock with the specified properties (next, address, privios, free, size).
- */
+
 void initailiztion()
 {
     /*Initialize baseList and headBlock to the current program break*/
@@ -34,7 +27,7 @@ void initailiztion()
     headBlock->address = (void*)headBlock;       // Address is set to the address of baseList
     headBlock->privios = NULL;                  // Previous pointer is set to NULL
     headBlock->free = FREE;                     // Block is marked as free
-    headBlock->size = SIZE_OF_SBRK_STEP;        // Size is set to SIZE_OF_SBRK_STEP
+    headBlock->size = SIZE_OF_SBRK_STEP - SIZE_OF_META_DATA;        // Size is set to SIZE_OF_SBRK_STEP
     
     // Set FirsttimeFlag to 1, indicating that initialization has been done for the first time
     FirsttimeFlag = 1;
@@ -94,11 +87,13 @@ free_list* SplitBlock(free_list* block,int size )
 void MerageTwoBlk(free_list* BlockOne,free_list* blockTwo)
 {
     /**change the next of the first block to the next of the second block */
+
     BlockOne->next = blockTwo->next;
     if(blockTwo->next != NULL)
-         blockTwo->next->privios = BlockOne->address;
+         blockTwo->next->privios = (free_list*)BlockOne->address;
     /**change the size of the the block whic is the sum of the sze of the two blocks and 32byte(size of meta data of the second block)*/
     BlockOne->size = BlockOne->size + blockTwo->size + SIZE_OF_META_DATA; 
+    
 
 }
 
@@ -114,10 +109,11 @@ void MerageTwoBlk(free_list* BlockOne,free_list* blockTwo)
  * @param size Size of the memory block to allocate.
  * @return Pointer to the allocated memory block, or NULL if allocation fails.
  */
+
 void* My_malloc(int size)
 {
     void* BloackAddress; /* Address of the allocated block */
-
+    size = ((size+7)/8)*8;
     /* If this is the first allocation, initialize the memory management system */
     if (FirsttimeFlag == 0)
     {
@@ -126,7 +122,7 @@ void* My_malloc(int size)
 
     /* Start from the head of the block list */
     current = headBlock;
-
+                
     /* Iterate through the block list */
     while (current)
     {
@@ -142,15 +138,18 @@ void* My_malloc(int size)
                 current->free = NOT_FREE;
                 /*updatre the size of the new block*/
                 current->size = size;
-                /*increasing the program breack since the previous block is the last free block*/
-                current->next = (free_list*)sbrk(SIZE_OF_SBRK_STEP);
-                /*update the size of the added block by th SIZE_OF_SBRK_STEP*/
-                current->next->size = SIZE_OF_SBRK_STEP;
-                /*linking the added block with the list and set its meata data */
-                current->next->address = current->next;
-                current->next->privios = current;
-                current->next->free = FREE;
-                current->next->next = NULL;
+                if(current->next == NULL)
+                {
+                    /*increasing the program breack since the previous block is the last free block*/
+                    current->next = (free_list*)sbrk(SIZE_OF_SBRK_STEP);
+                    /*update the size of the added block by th SIZE_OF_SBRK_STEP*/
+                    current->next->size = SIZE_OF_SBRK_STEP - SIZE_OF_META_DATA;
+                    /*linking the added block with the list and set its meata data */
+                    current->next->address = current->next;
+                    current->next->privios = current;
+                    current->next->free = FREE;
+                    current->next->next = NULL;
+                }
                 return BloackAddress;
             }
             /* If the block size is greater than the requested size plus metadata */
@@ -173,19 +172,34 @@ void* My_malloc(int size)
     }
 
     /* If no suitable block was found, allocate a new block */
-    LastLocation = sbrk(SIZE_OF_SBRK_STEP); /* Increase the program break */
+     /* Increase the program break */
     current = headBlock; /* Reset the current block to the head */
-
+    LastLocation = sbrk(SIZE_OF_SBRK_STEP);
     /* Iterate through the block list again */
     while (current)
     {
         if (current->next == NULL)
         {
             /* Increase the size of the last block */
-            current->size += SIZE_OF_SBRK_STEP;
-            /* Split the block */
-            free_list* tempBlock = SplitBlock(current, size);
-            BloackAddress = tempBlock->address + SIZE_OF_META_DATA; /* Adjust for metadata */
+            if(current->next == FREE)
+            {               
+                current->size += SIZE_OF_SBRK_STEP -SIZE_OF_META_DATA;
+                /* Split the block */
+                free_list* tempBlock = SplitBlock(current, size);
+                BloackAddress = tempBlock->address + SIZE_OF_META_DATA; /* Adjust for metadata */
+            }
+            else
+            {
+                
+                current->next = (free_list*)(LastLocation);
+                current->next->next=NULL;
+                current->next->privios = (free_list*)current->address;
+                current->next->free=FREE; 
+                current->next->size = SIZE_OF_SBRK_STEP - SIZE_OF_META_DATA;
+                free_list* tempBlock = SplitBlock(current->next, size);
+                BloackAddress = tempBlock->address + SIZE_OF_META_DATA; /* Adjust for metadata */
+            }
+
             return BloackAddress;
         }
 
@@ -207,49 +221,45 @@ void my_free(void* ptr)
 {
     /* Mark the block as free */
     free_list* Templist = (free_list*)(ptr-32);
-    Templist->free = FREE;
-
-
-    /**take dicession for merging the tow blocks*/
-
-    /*check if the next block if NULL or not */
-    if((void*)(Templist->next) != NULL)
+    if(ptr!=0)
     {
-        /*check if the next block if FREE or not */
-        if((Templist->next)->free == FREE)
-        {
-            MerageTwoBlk(Templist,Templist->next);
-        }
-    }
-    /*check if the previos block if NULL or not */
-    if(Templist->privios != NULL)
-    {
-        /*check if the previos block if FREE or not */
-        if((Templist->privios)->free == FREE )
-        {
-            MerageTwoBlk(Templist->privios ,Templist);
-        }   
-    }
-
+        if(Templist->free != FREE)
+            Templist->free = FREE;
+        else{
+            perror("rrrrrrrrr");
+        } 
 
     /* Start from the head of the block list */
     current = headBlock;
     while (current)
     {
+        if(current->next != NULL)
+        {
+            if (current->free==FREE && current->next->free==FREE)
+            {
+                MerageTwoBlk(current,current->next);
+            }
+        }
         /* Check if the current block is the last block */
         if (current->next == NULL)
         {
             /* If the last block is larger than the size of a single sbrk step, reduce the program break */
             if (current->size > SIZE_OF_SBRK_STEP)
             {
-                LastLocation = sbrk(SIZE_OF_SBRK_STEP * -1);
+                
+                
+                LastLocation = sbrk(SIZE_OF_SBRK_STEP* -1);
                 current->size -=  SIZE_OF_SBRK_STEP;
+                break;
             }
             //Templist = current->next;
         }
 
         current = current->next;
     }
+
+    }
+    
 }
 /**
  * @brief Allocates memory for an array of num elements of size bytes each and initializes the memory to zero.
@@ -262,14 +272,24 @@ void my_free(void* ptr)
  * @param size Size of each element, in bytes.
  * @return Pointer to the allocated memory block, or NULL if allocation fails.
  */
-void* my_calloc(int num,int size)
+void* my_calloc(size_t num,size_t size)
 {
+    void* ptr;
     long int totalSize = num * size;
-    void *ptr = My_malloc(totalSize);
-    if(ptr != NULL)
+    if(num == 0 )
     {
-        memset(ptr,0,totalSize);
+        return NULL;
     }
+    else if(totalSize < 0){
+        return NULL;
+    }
+    else{
+        ptr = malloc(totalSize);
+        
+        memset(ptr,0,totalSize);
+        
+    }
+        
     return ptr;
 
 }
@@ -287,11 +307,28 @@ void* my_calloc(int num,int size)
  */
 void* my_realloc(void* ptr, size_t newSize)
 {
-    void* newptr = My_malloc(newSize);
-    if(newptr != NULL)
+    void* newptr;
+    if(ptr == NULL)
     {
-        memcpy(newptr,ptr,((free_list*)(ptr-SIZE_OF_META_DATA))->size);
-        my_free(ptr);
+        newptr = malloc(newSize);
+    }
+    else if(0 == newSize && NULL!=ptr)
+    {
+        free(ptr);
+    }
+    else
+    {
+        
+        newptr = malloc(newSize);
+        if(newSize > ((free_list*)(ptr-32))->size)
+        {
+            memcpy(newptr,ptr,((free_list*)(ptr-32))->size);
+        }
+        else{
+            memcpy(newptr,ptr,newSize);
+        }
+        
+        free(ptr);
     }
     return newptr;
 
@@ -312,8 +349,9 @@ void DisplayBlocks()
         ptlist->privios,ptlist->size,ptlist->free);
         ptlist = ptlist->next;
     }
-    printf("\n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+    printf("\n##################################################################################################################################################################\n");
 }
+
 void* malloc(size_t size)
 {
     return My_malloc(size);
@@ -325,12 +363,130 @@ void free(void*ptr)
 
 void* realloc(void* ptr,size_t newsize)
 {
+    
     return my_realloc(ptr,newsize);
 }
 
 
 void* calloc(size_t num ,size_t size)
 {
+    
     return my_calloc(num,size);
 }
 
+/*
+#define NUM_ALLOCS 1000000
+#define MAX_SIZE 102400
+#define MAX_ITERATIONS 1000000
+
+void random_alloc_free_test() {
+    srand((unsigned int)time(NULL));
+    
+    void* pointers[NUM_ALLOCS] = {NULL};
+    
+    for (int i = 0; i < MAX_ITERATIONS; ++i) {
+        int index = rand() % NUM_ALLOCS;
+        if (pointers[index] == NULL) {
+            // Allocate memory
+            size_t size = (size_t)(rand() % MAX_SIZE) + 1;
+            pointers[index] = malloc(size);
+            if (pointers[index] != NULL) {
+                printf("Allocated memory of size %zu at address %p\n", size, pointers[index]);
+            } else {
+                fprintf(stderr, "Allocation failed for size %zu\n", size);
+            }
+        } else {
+            // Free memory
+            printf("Freeing memory at address %p\n", pointers[index]);
+            free(pointers[index]);
+            pointers[index] = NULL;
+        }
+    }
+    
+    // Free remaining allocated memory
+    for (int i = 0; i < NUM_ALLOCS; ++i) {
+        if (pointers[i] != NULL) {
+            printf("Freeing remaining memory at address %p\n", pointers[i]);
+            free(pointers[i]);
+            pointers[i] = NULL;
+        }
+    }
+}
+
+int main() {
+    printf("Starting random allocation and deallocation test...\n");
+    random_alloc_free_test();
+    printf("Test complete.\n");
+    return 0;
+}*/
+/*
+int main()
+{
+
+    int *ptr1 = (int*)malloc(100);
+    int *ptr2 = (int*)malloc(27);
+    int *ptr3 = (int*)malloc(600);
+
+
+
+int main(void)
+{
+    
+    printf("%p\n",sbrk(0));
+    int *ptr1 = (int*)My_malloc(50000);
+    int *ptr2 = (int*)My_malloc(50000);
+    
+    int *ptr3 = (int*)My_malloc(50000);
+    int *ptr4 = (int*)My_malloc(50000);
+    printf("%p\n",sbrk(0));
+    my_free(ptr1);
+    my_free(ptr2);
+    my_free(ptr3);
+    my_free(ptr4);
+
+    printf("%p\n",sbrk(0));
+
+    DisplayBlocks();
+
+    ptr4 = (int*)My_malloc(160);
+    ptr3 = (int*)My_malloc(7000);
+    my_free(ptr3);
+    my_free(ptr4);   
+    int*ptr5= (int*)My_malloc(3000);
+    ptr1=(int*)My_malloc(7000);
+    int*ptr6 = (int*)My_malloc(130000);
+    DisplayBlocks();
+    return 0;
+}
+*/
+/*
+int main()
+{
+
+    int *ptr[10000];
+    for (int  i = 0; i < 10000; i++)
+    {
+        
+    //if(i == 505)
+    //sleep(10);
+    //getchar();
+        ptr[i] = (int*)malloc(10240);
+        //printf("address[%d] = %p\n",i,ptr[i]);
+        
+        //if(ptr[i] == ptr[i-1] )
+        //getchar();
+
+    }
+    for (int j = 5000 - 1; j < 10000; j += 1)
+    {
+        if(j == 9998)
+        sleep(10);
+        free(ptr[j]);
+        //printf("address[%d] = %p\n",j,ptr[j]);
+    }
+    
+
+
+
+} */
+//gcc -o main.exe malloc.c && ./main.exe
